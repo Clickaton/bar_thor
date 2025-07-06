9644f9f087ce6ec3dc5d3e43f0ac9d6e61eaba65
chris@Thinkpad-t490 <christian.antognazza@gmail.com>
2025-07-06 13:06:59 -0300

    Agrego campo precio en productos de la base de datos y en el form de detalle-pedidos

---
+++ Thor_Bar/Clases/Caja.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thor_Bar
+{
+    internal class Caja
+    {
+        private DateTime fecha;
+        private decimal totalFacturado;
+        private decimal valorInicial;
+        private decimal cierreCaja;
+
+        public Caja(DateTime fecha, decimal totalFacturado, decimal valorInicial, decimal cierreCaja)
+        {
+            Fecha = fecha;
+            TotalFacturado = totalFacturado;
+            ValorInicial = valorInicial;
+            CierreCaja = cierreCaja;
+        }
+
+        public DateTime Fecha
+        {
+            get { return fecha; }
+            set { fecha = value; }
+        }
+
+        public decimal TotalFacturado
+        {
+            get { return totalFacturado; }
+            set { totalFacturado = value; }
+        }
+
+        public decimal ValorInicial
+        {
+            get { return valorInicial; }
+            set { valorInicial = value; }
+        }
+
+        public decimal CierreCaja
+        {
+            get { return cierreCaja; }
+            set { cierreCaja = value; }
+        }
+    }
+}
